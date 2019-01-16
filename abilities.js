364eb3bf31490cb0d4a9ce32aebd3b9f3a565e52
Kirk Scheibelhut <kjs@google.com>
2019-01-16 14:28:20 -0800

    Get abilities working

--- abilities.js
+++ abilities.js
@@ -1,11 +1,14 @@
 const dmg = require('dmgcalc');
 const Dex = require('../Pokemon-Showdown/sim/dex');
 
+var equal = require('deep-equal');
+
 const gen = Number(process.argv[2]) || 7;
 
 const previous = dmg.ABILITIES_BY_ID[gen - 1];
 const current = dmg.ABILITIES_BY_ID[gen];
 const dex = Dex.forFormat('gen' + gen + 'ou');
+const oldDex = Dex.forFormat('gen' + (gen - 1) + 'ou');
 
 const requiredKeys = {
   id: 1,
@@ -20,25 +23,50 @@ const optionalKeys = {
   isNonstandard: 1
 };
 
+console.log(Object.keys(current).length);
 
 const result = {};
 for (let id in current) {
-  if (!previous[id]) {
-    let val = dex.getAbility(id);
-    for (let k in val) {
-      const required = requiredKeys[k];
-      const optional = optionalKeys[k];
+  if (id === 'flashfireactivated') continue;
 
-      if (!(required || optional)) {
-        delete val[k];
-      } else if (required && !val[k]) {
-        console.error([val, k]);
-      } else if (optional && !val[k]) {
-        delete val[k];
-      }
+  let old = oldDex.getAbility(id);
+  let val = dex.getAbility(id);
+
+  delete val['gen'];
+
+  for (let k in val) {
+    const required = requiredKeys[k];
+    const optional = optionalKeys[k];
+
+    if (!(required || optional)) {
+      delete val[k];
+    } else if (required && !val[k]) {
+      console.error([val, k]);
+    } else if (optional && !val[k]) {
+      delete val[k];
     }
+  }
+
+  if (!previous[id]) {
     val.gen = gen;
     result[id] = val;
+  } else if (!equal(val, old)) {
+    let patch = {};
+    
+    for (let k in requiredKeys) {
+      if (val[k] && !equal(val[k], old[k])) {
+        patch[k] = val[k];
+      }
+    }
+    for (let k in optionalKeys) {
+      if (val[k] && !equal(val[k], old[k])) {
+        patch[k] = val[k];
+      }
+    }
+
+    if (!(Object.keys(patch).length === 0 && patch.constructor === Object)) {
+      result[id] = patch;
+    }
   }
 }
 
